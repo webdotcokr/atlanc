a773e8ccf1b473fcf5fc402c37c05aacc5e18fd6
webdotcokr <webdotcokr@gmail.com>
2025-08-29 20:19:33 +0900

    foreground 컬러 변경

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <div>
       {/* Main Hero Section */}
-      <section className="relative flex flex-col items-center justify-center h-[890px] max-md:h-[656px] w-full text-center bg-[url('/hero.webp')]">
+      <section className="relative flex flex-col items-center justify-center h-[600px] max-md:h-[656px] w-full text-center bg-[url('/hero.webp')]">
         <h2 className="en text-xl max-md:text-[14px] text-white">Atlanc Premium Men&apos;s Hair Consulting</h2>
         <p className="mt-6 text-white text-[55px] max-md:text-[30px] font-bold leading-[1.3] tracking-[-2]">우리는 헤어를 <br className="md:hidden"/>디자인하지 않습니다. <br/>당신의 인상을 디자인합니다.</p>
         {/* Bottom Center Text with Line */}
