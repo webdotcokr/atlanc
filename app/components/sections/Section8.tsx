37bc8bae1638255cb37c9ac03d18f16ea128ca00
yujung00 <ung5468@naver.com>
2025-12-02 17:22:17 +0900

    # section 8, section 9 반응형 작업

--- app/components/sections/Section8.tsx
+++ app/components/sections/Section8.tsx
@@ -3,34 +3,43 @@ import MotionWrapper from "../ui/motion-wrapper";
 export default function Section8() {
   return (
     <section className="bg-[#1FD854] relative">
-      <div className="w-full max-w-[1440px] pt-[120px] flex flex-col items-center  mx-auto">
+      <div className="w-full max-w-[1440px] pt-[120px] flex flex-col items-center  mx-auto max-md:pt-[60px]">
         <MotionWrapper animation="fadeInUp">
           <div className="flex flex-col gap-4 items-center">
-            <div className="px-2 py-1 bg-black text-white text-xl leading-[1.4] font-extrabold">
+            <div className="px-2 py-1 bg-black text-white text-xl leading-[1.4] font-extrabold max-md:text-base">
               Point 02
             </div>
-            <h1 className="text-[46px] leading-[1.3] text-center text-black">
+            <h1 className="text-[46px] leading-[1.3] text-center text-black max-md:text-2xl">
               따라서,{" "}
               <b className="font-extrabold">
                 아뜰랑의 고객 만족도는<br></br> 최고일 수 밖에 없습니다.
               </b>
             </h1>
-            <p className="text-2xl leading-[1.4] text-black text-center">
+            <p className="text-2xl leading-[1.4] text-black text-center max-md:text-[17px]">
               아뜰랑의 평균 재방률은 -%, <br></br>한 달 -명이 고객님이
               재방문합니다.
             </p>
           </div>
         </MotionWrapper>
+
+        <img
+          src="/review01.webp"
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 h-[851px] object-cover max-md:hidden"
+        />
+        <img
+          src="/review03.webp"
+          className="absolute bottom-0 left-1/2 -translate-x-1/2  w-full h-auto aspect-[375/412] object-cover md:hidden"
+        />
         <MotionWrapper animation="fadeInUp">
           <img
             src="/review02.webp"
-            className="w-[642px] h-[658px] object-cover"
+            className="w-[642px] h-[658px] object-cover max-md:w-1/2 mx-auto max-md:hidden"
+          />
+          <img
+            src="/review04.webp"
+            className="object-cover w-1/2 aspect-[199/360] mx-auto max-md:object-bottom md:hidden mt-[72px] relative z-[1]"
           />
         </MotionWrapper>
-        <img
-          src="/review01.webp"
-          className="absolute bottom-0 left-1/2 -translate-x-1/2 h-[851px] object-cover"
-        />
       </div>
     </section>
   );
--- app/components/sections/Section9.tsx
+++ app/components/sections/Section9.tsx
@@ -2,16 +2,16 @@ import MotionWrapper from "../ui/motion-wrapper";
 
 export default function Section9() {
   return (
-    <section className="py-[120px] bg-[#090D09]">
+    <section className="py-[120px] bg-[#090D09] max-md:py-[80px] max-md:!px-0">
       <div className="flex flex-col items-center w-full mx-auto">
         <div className=" flex flex-col items-center gap-4">
           <MotionWrapper animation="fadeIn">
-            <div className=" px-2 py-1 bg-[#26E45C] text-xl font-extrabold max-md:text-[16px]] text-center text-black w-fit">
+            <div className=" px-2 py-1 bg-[#26E45C] text-xl font-extrabold max-md:text-[16px] text-center text-black w-fit">
               Point 03
             </div>
           </MotionWrapper>
           <MotionWrapper animation="fadeInUp" delay={0.2}>
-            <p className=" text-white text-center text-[46px] max-md:text-[26px] leading-[1.3]  tracking-[-1.5]">
+            <p className=" text-white text-center text-[46px] max-md:text-2xl leading-[1.3]  tracking-[-1.5]">
               그렇기에 투자대비<br></br>
               <b className="font-extrabold">
                 독보적인 매출과 수익률이 가능합니다.
@@ -19,59 +19,69 @@ export default function Section9() {
             </p>
           </MotionWrapper>
           <MotionWrapper animation="fadeInUp" delay={0.2}>
-            <p className=" text-white text-center text-2xl  max-md:text-[26px] leading-[1.4]  tracking-[-1.5]">
-              아뜰랑 각 지점의 &apos;실제 월매출&apos;을<br></br> 거짓없이 투명하게
-              공개합니다.
+            <p className=" text-white text-center text-2xl  max-md:text-[17px] leading-[1.4]  tracking-[-1.5]">
+              아뜰랑 각 지점의 &apos;실제 월매출&apos;을<br></br> 거짓없이
+              투명하게 공개합니다.
             </p>
           </MotionWrapper>
         </div>
 
-        <MotionWrapper animation="fadeIn" className="mt-11">
-          <div className="flex relative pb-[78px] gap-5 justify-center items-center w-full overflow-hidden">
+        <MotionWrapper animation="fadeIn" className="mt-11 max-md:mt-10">
+          <div className="flex relative pb-[78px] gap-5 justify-center items-center w-full overflow-hidden max-md:flex-col max-md:gap-11">
             <img
               src="/store01.webp"
               alt="store"
-              className="w-[680px] h-[478px] opacity-[0.1]"
+              className="w-[680px] h-[478px] opacity-[0.1] max-md:hidden"
             ></img>
-            <img
-              src="/store03.webp"
-              alt="store"
-              className="w-[680px] h-[478px] mr-5"
-            ></img>
-            <img
-              src="/store04.webp"
-              className="w-[680px] h-[478px]"
-              alt="store"
-            ></img>
-            <img
-              src="/store02.webp"
-              alt="store"
-              className="w-[680px] h-[478px] opacity-[0.1]"
-            ></img>
+            <div className="relative max-md:pb-[330px] md:mr-5 shrink-0">
+              <img
+                src="/store03.webp"
+                alt="store"
+                className="w-[680px] md:h-[478px] max-md:w-full max-md:aspect-[404/280]"
+              ></img>
 
-            <div className="absolute flex gap-5 bottom-0 left-1/2 -translate-x-1/2">
               <img
                 src="/recept01.webp"
                 alt="recept"
-                className="w-[307px]"
-                style={{ boxShadow: "4px 12px 12px 0 rgba(0, 0, 0, 0.12)" }}
-              ></img>
-              <img
-                src="/recept02.webp"
-                alt="recept"
-                className="w-[307px]"
+                className="w-[307px] absolute md:top-[92px] md:left-[383px] max-md:bottom-0 max-md:left-1/2 max-md:-translate-x-1/2"
                 style={{ boxShadow: "4px 12px 12px 0 rgba(0, 0, 0, 0.12)" }}
               ></img>
             </div>
+
+            <div className="relative max-md:pb-[330px] shrink-0">
+              <img
+                src="/store04.webp"
+                className="w-[680px] md:h-[478px] md:mr-5 max-md:w-full max-md:aspect-[404/280]"
+                alt="store"
+              ></img>
+
+              <img
+                src="/recept02.webp"
+                alt="recept"
+                className="w-[307px] absolute md:top-[92px] md:right-[383px] max-md:bottom-0 max-md:left-1/2 max-md:-translate-x-1/2"
+                style={{ boxShadow: "4px 12px 12px 0 rgba(0, 0, 0, 0.12)" }}
+              ></img>
+            </div>
+
+            <img
+              src="/store02.webp"
+              alt="store"
+              className="w-[680px] h-[478px] opacity-[0.1] max-md:hidden"
+            ></img>
           </div>
         </MotionWrapper>
 
-        <MotionWrapper animation="fadeInUp" delay={0.2} className="mt-[87px]">
+        <MotionWrapper
+          animation="fadeInUp"
+          delay={0.2}
+          className="mt-[87px] max-md:pt-8"
+        >
           <div className="flex flex-col gap-4 items-center">
-            <h1 className="text-4xl leading-[1.3] font-bold text-[#26E45C] border-b-[3px] border-primary">
-              광고로 만든 일시적인 매출이 아닙니다.
+            <h1 className="text-4xl leading-[1.3] font-bold text-[#26E45C] border-b-[3px] max-md:border-b-[1px] border-primary max-md:text-2xl text-center">
+              광고로 만든 <br className="md:hidden"></br>일시적인 매출이
+              아닙니다.
             </h1>
-            <p className="text-2xl text-white leading-[1.4] text-center">
+            <p className="text-2xl text-white leading-[1.4] text-center max-md:text-[17px]">
               높은 만족도와 재방률을 바탕으로 <br></br>아뜰랑의 매출은 매달
               상승합니다.
             </p>
